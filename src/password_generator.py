626570bfb26fc2686d140cb75cd0aaa1e0c31ead
SpicyGengu <lovro.antic1@gmail.com>
2023-03-15 14:29:12 +0100

    pass gener
    yuo

---
+++ src/password_generator.py
@@ -0,0 +1,11 @@
+import random
+def createNewPassword(i):
+  usableCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+  passwordL = []
+  password = ''
+  for j in range(i):
+    passwordL.append(random.choice(usableCharacters))
+    password = password + passwordL[j]
+  return password
+
+#print(createNewPassword(20))
