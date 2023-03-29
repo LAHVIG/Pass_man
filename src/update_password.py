bbc5dbd17f4c1280a8c6e182045902d0f5c82e4f
SpicyGengu <lovro.antic1@gmail.com>
2023-03-29 15:16:11 +0200

    funktion til at update pass

---
+++ src/update_password.py
@@ -0,0 +1,4 @@
+def update_password(cursor, table, user, password, chategory):
+    cursor.execute("UPDATE " + table + " SET " + user + " = " + "'" + password + "'" + " WHERE Chategory = " + "'" + chategory + "'")
+
+update_password(2, "Passwords", "Lovro", "unicycle", "Netflix")
